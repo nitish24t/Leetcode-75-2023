fd80dbb735b4f9e91ffa0725585ae35d53bc56b1
Nitish Pandey <61662594+nitish24t@users.noreply.github.com>
2023-04-08 15:36:17 +0530

    Optimized solution - Taken

--- Isomorphic_Strings.py
+++ Isomorphic_Strings.py
@@ -1,26 +1,16 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        n = len(s)
-        mem_s = {}
-        mem_t = {}
-        for i in range(0,n):
-            if s[i] not in mem_s:
-                mem_s[s[i]] = [i]
-            else:
-                mem_s[s[i]].append(i)
-
-            if t[i] not in mem_t:
-                mem_t[t[i]] = [i]
-            else:
-                mem_t[t[i]].append(i)
-            # print("elems : {0}:{1}, \t count: {2},{3}".format(s[i], t[i], mem_s[s[i]], mem_t[t[i]]))
-            if mem_s[s[i]] != mem_t[t[i]]:
-                return False
-        # print(mem_s)
-        # print(mem_t)
-        if len(mem_s) != len(mem_t):
-            return False
-        return True
+        mem_s = []
+        mem_t = []
+        for elem in s:
+            mem_s.append(s.index(elem))
+        for elem in t:
+            mem_t.append(t.index(elem))
+        print(mem_s)
+        print(mem_t)
+        if mem_s == mem_t:
+            return True
+        return False
       
 
 """
